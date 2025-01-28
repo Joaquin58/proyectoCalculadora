a06e1e34b8e55d0278aef8fba920f13ef505f934
Joaquin58 <joaquin2rm@ciencias.unam.mx>
2025-01-28 17:39:12 -0600

    solucionado la recursion

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -2,12 +2,12 @@ package calculadora;
 
 public class Calculadora {
 
-    public static double suma(double a, double b, double ...c) {
+    public static double suma(double a, double b) {
         if (b == 0) {
-            return b;
+            return a;
         }
 
-        return a + suma(a , b-1);
+        return suma(a+1 , b-1);
     }
 
 
